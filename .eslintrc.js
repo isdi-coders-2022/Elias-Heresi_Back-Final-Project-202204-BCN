a38e76324dcaf23dee641caa05dd7e5fc6e2783c
Elías Heresi <elias.heresi@gmail.com>
2022-05-30 15:39:27 +0200

    Created basic routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   env: {
+    browser: true,
+    commonjs: true,
     es2021: true,
     node: true,
-    jest: true,
-    browser: true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
   },
   rules: {},
 };
