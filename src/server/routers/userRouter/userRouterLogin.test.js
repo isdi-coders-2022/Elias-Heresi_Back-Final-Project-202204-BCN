56f596bee1b1f65e4ab8d37612b1e2b117c8487f
Elías Heresi <elias.heresi@gmail.com>
2022-06-01 22:14:47 +0200

    Added error corner cases

--- src/server/routers/userRouter/userRouterLogin.test.js
+++ src/server/routers/userRouter/userRouterLogin.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const app = require("../..");
@@ -24,12 +25,12 @@ afterEach(async () => {
 });
 
 afterAll(async () => {
-  await mongoServer.stop();
+  mongoServer.stop();
   await mongoose.connection.close();
 });
 
 describe("Given a POST '/login' endpoint", () => {
-  describe("When it receives a request", () => {
+  describe("When it receives a valid request", () => {
     users = [
       {
         name: "test",
@@ -70,6 +71,40 @@ describe("Given a POST '/login' endpoint", () => {
         .send(userRequestReceived);
 
       expect(response.body.token).toBeDefined();
+    });
+  });
+  describe("When it receives a request with a non-existing user", () => {
+    test("Then an error with a 'User not found' message will be received", async () => {
+      const userRequestReceived = {
+        username: "no_existe",
+        password: "inexistente",
+      };
+
+      const response = await request(app)
+        .post("/user/login")
+        .send(userRequestReceived);
+
+      const expectedProperty = "msg";
+      const expectedMessage = "User not found";
+      expect(response.body).toHaveProperty(expectedProperty, expectedMessage);
+    });
+  });
+  describe("When it receives a request with an incorrect password", () => {
+    test("Then an error with a 'Incorrect username and/or password' message will be received", async () => {
+      const userRequestReceived = {
+        username: "andrea82",
+        password: "incorrect_password",
+      };
+
+      // bcrypt.compare = jest.fn().mockReturnValue(false);
+
+      const response = await request(app)
+        .post("/user/login")
+        .send(userRequestReceived);
+
+      const expectedProperty = "msg";
+      const expectedMessage = "Incorrect username and/or password";
+      expect(response.body).toHaveProperty(expectedProperty, expectedMessage);
     });
   });
 });
