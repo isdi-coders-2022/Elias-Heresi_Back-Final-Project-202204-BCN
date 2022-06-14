5b926780d48db0898b7434419c74f2af2caf0c06
Elías Heresi <elias.heresi@gmail.com>
2022-06-14 14:59:21 +0200

    Tested corner cases in controller

--- src/server/controllers/diaryControllers.test.js
+++ src/server/controllers/diaryControllers.test.js
@@ -1,4 +1,4 @@
-const { createEntry } = require("./diaryControllers");
+const { createEntry, editEntry } = require("./diaryControllers");
 
 describe("Given the createEntry controller", () => {
   describe("When invoked without the vitality property", () => {
@@ -20,6 +20,32 @@ describe("Given the createEntry controller", () => {
       const expectedError = new TypeError();
       expectedError.code = 403;
       expectedError.message = "Error creating entry";
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given the editEntry controller", () => {
+  describe("When invoked without the vitality property", () => {
+    test("Then a response containing the error will be received", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      const req = {
+        body: {
+          userId: { username: "Daddy Yankee" },
+        },
+      };
+
+      const next = jest.fn();
+      await editEntry(req, res, next);
+
+      const expectedError = new TypeError();
+      expectedError.code = 403;
+      expectedError.message = "Error editing entry";
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
